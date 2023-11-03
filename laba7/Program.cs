69723639f2669c0df7e836640aeac79478145a33
Вася <@DESKTOP-SFOGOU7>
2023-11-04 01:44:17 +0300

    Добавьте файлы проекта.

---
+++ laba7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("Inlet.in");
+
+        int shortestWordLength = -1;
+
+        foreach (string line in lines)
+        {
+            string[] words = line.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (shortestWordLength == -1 || word.Length < shortestWordLength)
+                {
+                    shortestWordLength = word.Length;
+                }
+            }
+        }
+
+        File.WriteAllText("Outlet.out", shortestWordLength.ToString());
+
+        Console.WriteLine("Результат записан в файл Outlet.out.");
+    }
+}
